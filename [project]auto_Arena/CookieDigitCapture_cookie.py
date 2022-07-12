69069f72c094b6a42c9a3bdd1c2d2549806d72d3
Sungwon Kang <69042750+sungwon-kang@users.noreply.github.com>
2022-07-13 02:23:59 +0900

    2022-07-13

--- [project]auto_Arena/CookieDigitCapture_cookie.py
+++ [project]auto_Arena/CookieDigitCapture_cookie.py
@@ -4,9 +4,10 @@ import numpy as np
 import pyautogui    # mouse, keyboard 관련 도구함 끌어오기
 import pyperclip    # 클립보드에 저장 혹은 클립보드에서 불러오기 위함
 import Imageprocessor_cookie as ip
+import winsound
 
 from PyQt5.QtWidgets import *
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt,QTimer
 from PyQt5.QtGui import *
 
 from tensorflow.keras.models import load_model
@@ -28,8 +29,11 @@ class DigitCapture(QMainWindow):
     x4=0
     y4=0
     
-    # 탐색할 전투력 최대값
-    # Max=0
+    refresh_x=0
+    refresh_y=0
+    
+    # AutoArena_x=0
+    # AutoArena_y=0
     
     # 전투력 P1, P2 좌표 저장 리스트
     saved_p1_cord=[]
@@ -39,8 +43,11 @@ class DigitCapture(QMainWindow):
     saved_p3_cord=[]
     saved_p4_cord=[]
     
+    Length=0
+    
     # 키 이벤트 락
     keyFlag=True
+    Thread_flag=True
     
     WindowWidth=600
     WindowHeight=450
@@ -59,11 +66,40 @@ class DigitCapture(QMainWindow):
         # 학습 모델 불러오기
         self.cnn=load_model('./0_models/cnn_v5_1.h5')
         
+        # 자동탐색
+        self.timer=QTimer()
+        self.timer.timeout.connect(self.run)
+    
+    def startThreadFunction(self):
+        ms=1000
+        self.timer.start(610*ms)
+        self.setText("[자동 탐색 시작]\n10분 10초마다 자동으로 마우스가 이동됩니다.\n주의하세요!")
+        self.btn_start.setText("멈춤(E)")
+        
+        self.Thread_flag=False
+        
+    def stopThreadFunction(self):
+        self.timer.stop()
+        self.append("자동 탐색 종료")
+        self.btn_start.setText("시작(E)")
+        
+        self.Thread_flag=True
+        
+    def ThreadFunction(self):
+        if self.Length>0 and Thread_flag==True:
+            self.startThreadFunction()
+        
+        elif self.Thread_falg==False:
+            self.stopThreadFunction()
+        else:
+            self.setText("등록된 좌표들이 없습니다.")
+            
     # 키 이벤트 캡처 기능
     def keyPressEvent(self, e):
         # 키 입력 제한 리스트
         printable=[Qt.Key_Q, Qt.Key_W, Qt.Key_R, Qt.Key_T, 
-                   Qt.Key_A, Qt.Key_E, Qt.Key_C, Qt.Key_X]
+                   Qt.Key_A, Qt.Key_E, Qt.Key_C, Qt.Key_X,
+                   Qt.Key_M]
         
         # 입력 키 가져오기
         key=e.key()
@@ -94,6 +130,17 @@ class DigitCapture(QMainWindow):
                 print("위치 값 ({}, {})\n".format(self.x4,self.y4))
                 self.setText("위치 입력 (x4, y4) ({}, {})".format(self.x4,self.y4))
             
+            # elif key == Qt.Key_N:
+            #     self.AutuArena_x, self.AutuArena_y = pyautogui.position()
+                
+            #     print("프로그램 위치 값 ({}, {})\n".format(self.AutuArena_x, self.AutuArena_y))
+            #     self.setText("프로그램 위치 ({}, {})\n".format(self.AutuArena_x, self.AutuArena_y))
+                
+            elif key == Qt.Key_M:
+                self.refresh_x, self.refresh_y = pyautogui.position()
+                
+                print("새로고침 위치 값 ({}, {})\n".format(self.refresh_x, self.refresh_y))
+                self.setText("새로고침 위치 ({}, {})\n".format(self.refresh_x, self.refresh_y))
                 
             elif key == Qt.Key_A: 
                 # 저장된 좌표 P1, P2 리스트에 추가
@@ -109,7 +156,9 @@ class DigitCapture(QMainWindow):
                 
                 self.setText("리스트 추가됨 \np1 - "+str(self.saved_p1_cord) +"\np2 - "+ str(self.saved_p2_cord))
                 self.te_event.append("\np3 - "+str(self.saved_p3_cord) +"\np4 - "+ str(self.saved_p4_cord))
-            
+                
+                self.Length+=1
+                
             elif key == Qt.Key_C:
                 # 좌표 리스트 초기화
                 self.saved_p1_cord.clear()
@@ -117,17 +166,21 @@ class DigitCapture(QMainWindow):
                 self.saved_p3_cord.clear()
                 self.saved_p4_cord.clear()
                 
+                self.Length=0
                 print("list clear")
                 self.setText("리스트 초기화됨")
                 
             elif key == Qt.Key_E:
                 self.run()
-                
+                    
             elif key == Qt.Key_X:
                 self.quitFunction()
                 
     def run(self):
         self.keyFlag=False
+        
+        # pyautogui.click(self.refresh_x, self.refresh_y)
+        # time.sleep(0.5)
         
         # 캡쳐
         powerImgs=self.CaptureFunction(self.saved_p1_cord, self.saved_p2_cord)
@@ -135,8 +188,6 @@ class DigitCapture(QMainWindow):
         
         # 분할
         cut, powerDigit, throphyDigit, fx1, fx2 = self.getParameterTopLine()
-        # 리스트가 아닌 각 이미지를 전달해야함.
-        # 리스트로 전달하게 만들기
         cropped_powerImgs=IP.crop(lst_cvimgs=powerImgs, cut=cut, crop_n=powerDigit, crop_fx1=fx1, crop_fx2=fx2)
         cropped_trophyImgs=IP.crop(lst_cvimgs=throphyImgs, cut=cut, crop_n=throphyDigit, crop_fx1=fx1, crop_fx2=fx2)
         
@@ -148,53 +199,64 @@ class DigitCapture(QMainWindow):
         powerNums=self.predictFunction(proceed_powerImgs)
         throphyNums=self.predictFunction(proceed_trophyImgs)
         
+        opponents=self.findOppsUCanwin(powerNums,throphyNums)
         
-        self.setText(powerNums)
-        self.append(throphyNums)
+        if(len(opponents)>0):
+            winsound.MessageBeep(-1)
+            self.setText("상대 발견")
+            self.stopThreadFunction()
+            
         self.keyFlag=True
+        
+    def findOppsUCanwin(self, lstPowerNums,lstThrophyNums):
+        boundThrophy,boundMax=self.getParameterMiddleLine()
+        
+        n = self.Length
+        opponents=[]
+        #? 왜? OOR error? WHY?
+        for i in range(n):
+            Power=lstPowerNums[i]
+            Throphy=lstThrophyNums[i]
+            
+            if(boundThrophy>=Throphy or boundMax>=Power):
+                opponents.append((Throphy,Power))
+        
+        return opponents
         
     def CaptureFunction(self, l1, l2):
         # 스크린샷 좌표 가져오기 
-        p1=len(l1)
-        p2=len(l2)
-        
-        isSame = (p1==p2)
-       
-        if( isSame == True and l1!=0 ):
-            n = len(l1)
+        n = self.Length
+        # 추가된 좌표 쌍 갯수 만큼 반복
+        captured_imgs=[]
+        for i in range(n):
+            (x1,y1)=l1[i]
+            (x2,y2)=l2[i]
+            print("전투력 위치 ({}, {}), ({}, {})".format(x1,y1,x2,y2))
             
-            # 추가된 좌표 쌍 갯수 만큼 반복
-            captured_imgs=[]
-            for i in range(n):
-                (x1,y1)=l1[i]
-                (x2,y2)=l2[i]
-                print("전투력 위치 ({}, {}), ({}, {})".format(x1,y1,x2,y2))
-                
-                X, Y, Width, Height = self.getCaptureCoord(x1,x2,y1,y2)
-                print("영역 : ({}, {}, {}, {})\n".format(X, Y, Width, Height))
-                
-                captured_imgs.append(pyautogui.screenshot(region=(X, Y, Width, Height)))
+            X, Y, Width, Height = self.getCaptureCoord(x1,x2,y1,y2)
+            print("영역 : ({}, {}, {}, {})\n".format(X, Y, Width, Height))
             
-            return captured_imgs
+            captured_imgs.append(pyautogui.screenshot(region=(X, Y, Width, Height)))
+        
+        return captured_imgs
         
         
-    def predictFunction(self, lst_imgs):
+    def predictFunction(self, lstimgs):
         
-        if len(lst_imgs)==0:
-            self.setText("등록된 이미지가 없습니다.")
-        else:
-            # 이미 전처리된 이미지를 파라미터로 받는다.
-            # 처리된 이미지들을 모델 입력 데이터 형태로 변환
-            for imgs in lst_imgs:
-                predNumList=[]
-                arr_imgs=IP.listToArray(imgs,'float32')
-                res=self.cnn.predict(arr_imgs)
-                result=self.getNumber(res)
-                print("예측 값:", result)
-                    
-                predNumList.append(result)
-                    
-            return predNumList
+        # 이미 전처리된 이미지를 파라미터로 받는다.
+        # 처리된 이미지들을 모델 입력 데이터 형태로 변환
+        for imgs in lstimgs:
+            predNumList=[]
+            arr_imgs=IP.listToArray(imgs,'float32')
+            res=self.cnn.predict(arr_imgs)
+            result=self.getNumber(res)
+            print("예측 값:", result)
+                
+            predNumList.append(result)
+                
+        return predNumList
+        
+            
         
     def getNumber(self, predNums):
         # 각 자릿수를 계산
@@ -219,11 +281,12 @@ class DigitCapture(QMainWindow):
                                 '  <R>를 누를 시 마우스 포인터 기준으로 x3, y3이 저장됩니다.\n'+
                                 '  <T>를 누를 시 마우스 포인터 기준으로 x4, y4이 저장됩니다.\n'+
                                 '  <A>를 누를 시 단축키로 설정한 모든 좌표를 리스트에 추가합니다.\n'+
-                                '  <P>를 누를 시 수를 예측합니다.\n'+
+                                '  <M>을 눌러 [무료 새로고침] 버튼의 좌표 저장하세요.\n'
+                                '  <E>를 누를 시 자동탐색이 시작됩니다.\n'+
                                 '  <X>를 누를 시 프로그램이 종료됩니다.\n\n'+
                                 
                                 '[사용법]\n'+
-                                '  1. <Q>와 <W>, <R>와 <T>를 눌러 사각형 좌표를 얻고 <A>를 눌러 리스트에 추가합니다.\n'+
+                                '  1. 전투력 이미지 <Q>와 <W>, 트로피 이미지 <R>와 <T>를 눌러 사각형 좌표를 얻고 <A>를 눌러 리스트에 추가합니다.\n'+
                                 '  2. (1)을 반복해서 여러 좌표를 얻을 수 있습니다.\n'+
                                 '  3. <E> 또는 [시작] 버튼을 눌러 리스트에 저장된 좌표 쌍의 수만큼 캡쳐되고, 수를 예측합니다.\n'
                                 '  4. 예측된 수가 UI에 출력됩니다.\n\n'
@@ -233,10 +296,11 @@ class DigitCapture(QMainWindow):
                                 ' 분할 이미지 왼쪽 너비 조절: 각 분할 이미지의 좌변 너비 크기 설정값\n(기본값 :0)\n'
                                 ' 분할 이미지 오른쪽 너비 조절: 각 분할 이미지의 우변 너비 크기 설정값\n(기본값 :0)\n'
                                 ' 분할 수: 캡쳐 이미지 분할 수 설정값\n(기본값 :7)\n'
+                                # '[숙지바람] 금전적 손해가 발생할 시 책임 없음.'
                                 )
-    def processImgs(self, lst_imgs):
+    def processImgs(self, lstimgs):
         listofImgs=[]
-        for imgs in lst_imgs:
+        for imgs in lstimgs:
             proceed_imgs=[]
             for img in imgs:
                 procced_img = IP.preprocessing(img)
@@ -256,12 +320,6 @@ class DigitCapture(QMainWindow):
     def quitFunction(self):
         self.close()
     
-    def printInList(self):
-        # 
-        # Max, throphy = self.getParameterMiddleLine()
-        # lst = [i for i in lst if i <= self.Max]
-        self.te_event.append("예측된 수: "+str(self.predicted_list))
-        self.te_event.append("\n나보다 낮은 전투력들: "+str(self.findInList(self.predicted_list)))
         
     def getCaptureCoord(self, x1, x2, y1,y2):
         # 좌표 정렬 후 차이를 구하여 너비와 높이를 계산
@@ -285,7 +343,7 @@ class DigitCapture(QMainWindow):
         throphy = int(self.le_EnemyMaxtrophy.text())
         Max = int(self.le_EnemyMaxPower.text())
         
-        return Max, throphy
+        return throphy, Max
 
     def setText(self, msg):
         self.te_event.clear()
@@ -387,13 +445,13 @@ class DigitCapture(QMainWindow):
         self.le_EnemyMaxtrophy = QLineEdit(self)
         self.le_EnemyMaxtrophy.setValidator(QIntValidator(self))
         self.le_EnemyMaxtrophy.setAlignment(Qt.AlignCenter)
-        self.le_EnemyMaxtrophy.setText('0')
+        self.le_EnemyMaxtrophy.setText('2900')
         
         lb_EnemyMaxPower = QLabel('탐색할 상대 전투력 최대치', self)
         self.le_EnemyMaxPower = QLineEdit(self)
         self.le_EnemyMaxPower.setValidator(QIntValidator(self))
         self.le_EnemyMaxPower.setAlignment(Qt.AlignCenter)
-        self.le_EnemyMaxPower.setText('0')
+        self.le_EnemyMaxPower.setText('1200000')
         
         lb_middle.setGeometry(0,149,self.WindowWidth, 30)
         
@@ -414,19 +472,19 @@ class DigitCapture(QMainWindow):
         self.te_event = QTextEdit('사용법을 확인하세요!', self)
         self.te_event.setReadOnly(True)
         
-        btn_start = QPushButton('시작(E)', self)
+        self.btn_start = QPushButton('시작(E)', self)
         btn_reset = QPushButton('설정 초기화',self)
         btn_guide = QPushButton('사용법', self)       
         btn_quit = QPushButton('종료', self)
         
         self.te_event.setGeometry(10, self.WindowHeight-140, 580, 90)
-        btn_start.setGeometry(10, self.WindowHeight-40, 100, 30)
+        self.btn_start.setGeometry(10, self.WindowHeight-40, 100, 30)
         btn_reset.setGeometry(120, self.WindowHeight-40, 100, 30)
         btn_guide.setGeometry(380, self.WindowHeight-40, 100, 30)
         btn_quit.setGeometry(490, self.WindowHeight-40, 100, 30)
         
         layout_bottom.addWidget(self.te_event)
-        layout_bottom.addWidget(btn_start)
+        layout_bottom.addWidget(self.btn_start)
         layout_bottom.addWidget(btn_reset)
         layout_bottom.addWidget(btn_guide)
         layout_bottom.addWidget(btn_quit)
@@ -436,7 +494,7 @@ class DigitCapture(QMainWindow):
         main_layout.addWidget(frame_bottom)
         
         # 각 버튼에 콜백함수 연결
-        btn_start.clicked.connect(self.CaptureFunction)
+        self.btn_start.clicked.connect(self.ThreadFunction)
         btn_reset.clicked.connect(self.resetFunction)
         btn_guide.clicked.connect(self.showGuideFunction)
         btn_quit.clicked.connect(self.quitFunction)
